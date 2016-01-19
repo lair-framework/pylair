bca4a32334f1f9be1a9042dc7cc19b84c5820d83
Tom Steele <thomasjsteele@gmail.com>
2016-01-19 12:51:28 -0700

    1.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name="pylair",
-    version="1.0.1",
+    version="1.0.2",
     author='Dan Kottmann',
     author_email='djkottmann@gmail.com',
     packages=['pylair'],
